480210596b1e217ab9e53f3ca432c5ad1af2be03
Maxwell <thetaung@nuzay.com>
2020-11-30 22:19:18 +0630

    pwa

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,6 +4,7 @@ const SearchBox = ({searchField,searchChange}) => {//calling the searchChange pr
     return (                                       //so whenever onChange occurs in the input, searchChange function got called
         <div className="pa2">
             <input
+                aria-label="search robots"
                 className="pa3 ba b--green bg-lightest-blue"
                 type="search"
                 placeholder="search robots"
